b7176422be57a70790bd80592ddf09c61718eb24
peteherman <peteherman@yahoo.com>
2022-11-12 11:58:43 -0500

    replaced server uri string with const

--- server.go
+++ server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+const SERVER_PORT = 8080
 
 func main() {
 	// Create new base Mux
@@ -15,9 +18,12 @@ func main() {
 	// Add logger middleware
 	loggerMux := NewLoggerMiddleware(mux)
 
-	// Start the webserver
-	log.Println("Starting Web Server")
-	if err := http.ListenAndServe(":8080", loggerMux); err != nil {
+	// Configure webserver uri
+	server_uri := fmt.Sprintf(":%v", SERVER_PORT)
+	
+	// Start the webserver	
+	log.Println("Starting Web Server")	
+	if err := http.ListenAndServe(server_uri, loggerMux); err != nil {
 		log.Fatal(err)
 	}
 }
